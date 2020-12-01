41c349b2b63026c34d99d43d25f4397156de6c70
김민서 <minseokim@gimminseoui-MacBookPro.local>
2020-12-01 20:52:17 +0900

    Add: models.py 작성

--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from core      import models as core_models
+
+class User(core_models.TimeStampModel):
+    name   = models.CharField(max_length=100)
+    email  = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.name
+
+class Favor(core_models.TimeStampModel):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    room = models.ForeignKey('room.Room', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'favors'
