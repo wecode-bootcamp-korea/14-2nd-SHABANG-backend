6065edbd7584e8910bc6a8b3f5b8775bb9ae57ff
jeongsik <summering6@gmail.com>
2020-12-08 16:34:23 +0900

    fixed: conflict problem

--- facility/tests.py
+++ facility/tests.py
@@ -1,3 +1,138 @@
 from django.test import TestCase
+from django.test import Client
+from unittest.mock import patch, MagicMock
 
-# Create your tests here.
+from .models import School, Subway, ConvenientStore, Cafe, SchoolGrade, SubwayLine
+from apartment.models import ApartmentComplex
+
+class FacilityTest(TestCase):
+
+    def setUp(self):
+        client = Client()
+        
+        ApartmentComplex.objects.create(
+            id               = 99,
+            name             = 'wecode아파트',
+            household_number = 1234,
+            completion_year  = 2020,
+            address          = '서울시 위코드동',
+            latitude         = 37.518740000000001,
+            longitude        = 127.020550000000001
+        )
+
+        ApartmentComplex.objects.create(
+            id               = 98,
+            name             = 'wecode아파트2',
+            household_number = 1234,
+            completion_year  = 2020,
+            address          = '서울시 위코드동',
+            latitude         = 137.518740000000001,
+            longitude        = 1127.020550000000001
+        )
+
+        SchoolGrade.objects.create(
+            id    = 1,
+            grade = '초'
+        )
+
+        School.objects.create(
+            id              = 1,
+            name            = '위코드초등학교',
+            latitude        = str('37.523433300000005'),
+            longitude       = str('127.024074900000006'),
+            school_grade_id = 1
+        )
+
+        SubwayLine.objects.create(
+            id   = 1,
+            line = '위코드라인'
+        )
+
+        Subway.objects.create(
+            id             = 1,
+            name           = '위코드역',
+            latitude       = str('37.523433300000005'),
+            longitude      = str('127.024074900000006'),
+            subway_line_id = 1
+        )
+
+        ConvenientStore.objects.create(
+            id        = 1,
+            name      = '위코드CU',
+            latitude  = str('37.523433300000005'),
+            longitude = str('127.024074900000006')
+        )
+
+        Cafe.objects.create(
+            id        = 1,
+            name      = '위코드스타벅스',
+            latitude  = str('37.523433300000005'),
+            longitude = str('127.024074900000006')
+        )
+
+    def tearDown(self):
+        School.objects.all().delete()
+        Subway.objects.all().delete()
+        ConvenientStore.objects.all().delete()
+        Cafe.objects.all().delete()
+
+    def test_getting_near_facilities_success(self):
+        client   = Client()
+        response = client.get('/facility/99')
+
+        self.assertEquals(response.json(),
+        {
+            "school": [
+                {
+                    "id"       : 1,
+                    "name"     : "위코드초등학교",
+                    "latitude" : str('37.523433300000005'),
+                    "longitude": str('127.024074900000006'),
+                    "distance" : 607
+                }
+            ],
+            "subway": [
+                {
+                    "id"       : 1,
+                    "name"     : "위코드역",
+                    "latitude" : str('37.523433300000005'),
+                    "longitude": str('127.024074900000006'),
+                    "distance" : 607
+                }
+            ],
+            "convenient_store": [
+                {
+                    "id"       : 1,
+                    "name"     : "위코드CU",
+                    "latitude" : str('37.523433300000005'),
+                    "longitude": str('127.024074900000006'),
+                    "distance" : 607
+                }
+            ],
+            "cafe": [
+                {
+                    "id"       : 1,
+                    "name"     : "위코드스타벅스",
+                    "latitude" : str('37.523433300000005'),
+                    "longitude": str('127.024074900000006'),
+                    "distance" : 607
+                }
+            ]
+        }
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_getting_near_facilities_fail(self):
+        client   = Client()
+        response = client.get('/facility/98')
+        self.assertEqual(response.json(),
+        {
+            "message":"INVALID_COORDINATE"
+        }
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_getting_near_facilities_not_found(self):
+        client   = Client()
+        response = client.get('/facility?complex=99')
+        self.assertEqual(response.status_code, 404)
--- facility/views.py
+++ facility/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from haversine import haversine
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+from django.db.models import Q
+
+from facility.models import School, Subway, ConvenientStore, Cafe
+from apartment.models import ApartmentComplex
+
+class NearFacilityView(View): 
+    def get(self, request, id):
+        try:
+            apartment_complex = ApartmentComplex.objects.get(id=id)
+            latitude = float(apartment_complex.latitude)
+            longitude = float(apartment_complex.longitude)
+            position      = (latitude,longitude)
+            LATITUDE_1KM  = 0.00904
+            LONGITUDE_1KM = 0.00898
+            if not (-90 < latitude < 90 and -180 < longitude < 180):
+                return JsonResponse({"message":"INVALID_COORDINATE"}, status=400)
+            limit = (
+                Q(latitude__gt=latitude-LATITUDE_1KM, latitude__lt=latitude+LATITUDE_1KM) &
+                Q(longitude__gt=longitude-LONGITUDE_1KM, longitude__lt=longitude+LONGITUDE_1KM)
+            )
+
+            schools = School.objects.filter(limit)
+            near_schools = [school for school in schools if haversine(position, (school.latitude, school.longitude)) < 1 ]
+            school_data = [{
+                "id"        : school.id,
+                "name"      : school.name,
+                "latitude"  : school.latitude,
+                "longitude" : school.longitude,
+                "distance"  : int(haversine(position, (school.latitude, school.longitude), unit='m'))
+            }for school in near_schools]
+
+            subways = Subway.objects.select_related("subway_line").filter(limit)
+            near_subways = [subway for subway in subways if haversine(position, (subway.latitude, subway.longitude)) < 1]
+            subway_data = [{
+                "id"        : subway.id,
+                "name"      : subway.name,
+                "latitude"  : subway.latitude,
+                "longitude" : subway.longitude,
+                "distance"  : int(haversine(position, (subway.latitude, subway.longitude), unit='m'))
+            }for subway in near_subways]
+
+            convenient_stores = ConvenientStore.objects.filter(limit)
+            near_convenient_stores = [store for store in convenient_stores if haversine(position, (store.latitude, store.longitude)) < 1]
+            convenient_store_data = [{
+                "id"        : store.id,
+                "name"      : store.name,
+                "latitude"  : store.latitude,
+                "longitude" : store.longitude,
+                "distance"  : int(haversine(position, (store.latitude, store.longitude), unit='m'))
+            }for store in near_convenient_stores]
+
+            cafes = Cafe.objects.filter(limit)
+            near_cafes = [cafe for cafe in cafes if haversine(position, (cafe.latitude, cafe.longitude)) < 1]
+            cafe_data = [{
+                "id"        : cafe.id,
+                "name"      : cafe.name,
+                "latitude"  : cafe.latitude,
+                "longitude" : cafe.longitude,
+                "distance"  : int(haversine(position, (cafe.latitude, cafe.longitude), unit='m'))
+            }for cafe in near_cafes]
+
+            return JsonResponse(
+                {
+                    "school"           : school_data,
+                    "subway"           : subway_data,
+                    "convenient_store" : convenient_store_data,
+                    "cafe"             : cafe_data,
+                    }
+                    ,status=200)
+        except ValueError:
+            return JsonResponse({"message":"INVALID_VALUE"}, status=400)
+        except TypeError:
+            return JsonResponse({"message":"INVALID_TYPE"}, status=400)
+
