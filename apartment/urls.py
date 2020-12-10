28bb509780856d83b9d0e4a9412a2fd066959a5e
gemma-Kim <gemmak.rok@gmail.com>
2020-12-10 21:28:45 +0900

    Add: ComplexDetailView, ApartmentMapView, apartment.tests.

--- apartment/urls.py
+++ apartment/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from apartment.views import ApartmentGraphView
+from apartment.views import ApartmentGraphView, ComplexDetailView, ApartmentMapView
 
 urlpatterns = [
-    path('/graph/<int:id>', ApartmentGraphView.as_view())
+    path('/graph/<int:id>', ApartmentGraphView.as_view()),
+    path('/complex/<int:complex_id>', ComplexDetailView.as_view()),
+    path('/map', ApartmentMapView.as_view()),
 ]
